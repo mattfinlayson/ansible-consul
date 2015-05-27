733956ea95ccd67afa3673666c199a4b86efaf18
Travis Truman <travis_truman@cable.comcast.com>
2015-05-27 13:52:20 -0400

    Setting GOMAXPROCS to the number of processors on the host

--- test/integration/default/serverspec/consul_spec.rb
+++ test/integration/default/serverspec/consul_spec.rb
@@ -11,6 +11,11 @@ describe 'Consul' do
     its(:content) { should match /"server": true/ }
   end
 
+  describe file('/etc/init/consul.conf') do
+    it { should be_file }
+    its(:content) { should match /export GOMAXPROCS=`nproc`/ }
+  end
+
   describe file('/var/log/consul') do
     it { should be_file }
   end
