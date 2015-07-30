4179744d356dc2bfef60ee7c782f5dbe59775ef3
rbruta200 <roel_brutas@comcast.com>
2015-07-30 10:54:46 -0400

    adding test

--- test/integration/tls/serverspec/consul_tls_spec.rb
+++ test/integration/tls/serverspec/consul_tls_spec.rb
@@ -42,6 +42,6 @@ describe 'Consul with TLS enabled' do
 
   describe file('/etc/init/consul.conf') do
     it { should be_file }
-    its(:content) { should match /sudo setcap CAP_NET_BIND_SERVICE=+eip \/opt\/consul\/bin\/consul/ }
+    its(:content) { should match /sudo setcap CAP_NET_BIND_SERVICE=\+eip \/opt\/consul\/bin\/consul; exec sudo -u consul -g consul \/opt\/consul\/bin\/consul agent \\/ }
   end
 end
